5faee4090de6781e1a5c93dea57e4e3860b509bd
Michael Engen <mengen@paypal.com>
2025-01-30 15:38:33 -0500

    Working guest checkout without billing address

--- src/static/checkout-fastlane-flexible.js
+++ src/static/checkout-fastlane-flexible.js
@@ -1,4 +1,12 @@
-import { addApiCalls, getOptions, setAuthHeader } from "./utils.js";
+import {
+  addApiCalls,
+  getOptions,
+  setOptions,
+  loadOptions,
+  setAuthHeader,
+  saveOptionsAndReloadPage,
+} from "./utils.js";
+import { buildScriptElement, setupEventListeners } from "./checkout.js";
 
 async function createOrder(singleUseToken) {
   console.group("Creating the order...");
@@ -63,74 +71,14 @@ async function captureOrder({ orderId, authId }) {
   console.groupEnd();
 }
 
-let fastlane;
-let fastlanePaymentComponent;
-let identity, profile;
+// let fastlane;
+// let fastlanePaymentComponent;
+// let identity, profile;
 const styles = {
   // root: { backgroundColorPrimary: "#FAFAFA" }
   // root: { backgroundColorPrimary: "#FFF" }
   root: { backgroundColorPrimary: "transparent" },
 };
-
-async function attemptCheckout() {
-  console.group("Attempting checkout...");
-  if (!fastlanePaymentComponent) {
-    console.log(
-      "The fastlanePaymentComponent is not yet loaded. Load the component by looking up an email first.",
-    );
-    alert("Fastlane checkout first requires an email address lookup!");
-    console.groupEnd();
-    return;
-  }
-
-  console.log("Facilitating checkout...");
-  const { id: paymentTokenId } = await fastlanePaymentComponent.getPaymentToken();
-
-  const { orderId, authId, captureId } = await createOrder(paymentTokenId);
-
-  if (captureId) {
-    console.log(`Received capture back from 'create order': ${captureId}`);
-  } else {
-    console.log("Attempting capture...");
-    await captureOrder({ orderId, authId });
-  }
-}
-
-async function initializeCheckout() {
-  const emailContainer = document.getElementById("fastlane-email-container");
-  const emailButton = document.getElementById("fastlane-email-button");
-
-  const emailInput = document.getElementById("fastlane-email-input");
-  const emailAddress = emailInput?.value;
-
-  if (emailAddress) {
-    emailContainer.setAttribute("hidden", true);
-    emailContainer.setAttribute("disabled", true);
-    emailInput.setAttribute("disabled", true);
-    emailButton.classList.toggle("disabled");
-
-    const { customerContextId } = await identity.lookupCustomerByEmail(emailAddress);
-
-    if (customerContextId) {
-      await initializeReturnBuyerCheckout(customerContextId);
-    } else {
-      await initializeGuestBuyerCheckout();
-    }
-  } else {
-    alert("No email found to look up!");
-  }
-
-  if (fastlanePaymentComponent) {
-    await fastlanePaymentComponent.render("#payment-container");
-
-    const fastlaneWatermark = document.getElementById("watermark-container");
-    fastlaneWatermark.classList.toggle("hidden");
-
-    const payButton = document.getElementById("pay-button");
-    payButton.classList.toggle("disabled");
-  }
-}
-
 class FastlaneFlexibleCheckout {
   config = {
     cardOptions: { allowedBrands: ["VISA"] },
@@ -141,184 +89,178 @@ class FastlaneFlexibleCheckout {
     window.localStorage.setItem("fastlaneEnv", "sandbox");
   }
 
+  get emailInput() {
+    return document.getElementById("fastlane-email-input");
+  }
+  get emailContainer() {
+    return document.getElementById("fastlane-email-container");
+  }
+  get emailAddress() {
+    return this.emailInput?.value;
+  }
+  get watermarkId() {
+    return "watermark-container";
+  }
+  get watermarkContainer() {
+    return document.getElementById(this.watermarkId);
+  }
+
   async initializeFastlane() {
     console.group("Initializing Fastlane...");
-    console.log(`Configuration: ${this.config}`);
+    console.log("Configuration: ", this.config);
 
     this.fastlane = await paypal.Fastlane(this.config);
     console.log({ fastlane: this.fastlane });
+
+    document.getElementById("fastlane-email-button").removeAttribute("disabled");
+
+    this.initializeWatermark();
     console.groupEnd();
   }
 
   async initializeWatermark() {
+    const includeAdditionalInfo = true;
+    console.log(
+      "Initializing watermark, " +
+        (includeAdditionalInfo ? "" : "not ") +
+        "including additional info.",
+    );
     this.watermark = await this.fastlane.FastlaneWatermarkComponent({
-      includeAdditionalInfo: true,
+      includeAdditionalInfo,
     });
-    this.watermark.render("#watermark-container");
+    this.watermark.render(`#${this.watermarkId}`);
   }
 
-  submitEmail() {
+  async attemptEmailLookup() {
     const { emailAddress } = this;
     if (emailAddress) {
       const { emailContainer } = this;
       emailContainer.setAttribute("disabled", true);
       emailContainer.setAttribute("hidden", true);
+      await this.lookupEmail(emailAddress);
     } else {
       alert("No email found to look up!");
     }
   }
 
-  lookupEmail() {}
+  async lookupEmail(emailAddress) {
+    const { identity } = this.fastlane;
+    const { customerContextId } = await identity.lookupCustomerByEmail(emailAddress);
 
-  get emailInput() {
-    return document.getElementById("fastlane-email-input");
-  }
+    this.watermarkContainer.classList.toggle("hidden", true);
 
-  get emailContainer() {
-    return document.getElementById("fastlane-email-container");
-  }
-
-  get emailAddress() {
-    return this.emailInput?.value;
-  }
-}
-
-async function initializeReturnBuyerCheckout(customerContextId) {
-  console.group("Initializing return buyer (Ryan) flow...");
-  console.log({ customerContextId });
-
-  const { authenticationState, profileData } =
-    await identity.triggerAuthenticationFlow(customerContextId);
-  console.log("Authentication result:", { authenticationState, profileData });
-  if (authenticationState === "succeeded") {
-    console.log("Authentication succeeded!");
-    // const { shippingAddress } = profileData
-    // if (shippingAddress) {
-    //     initializeShippingAddress()
-    //     displayShippingAddress(shippingAddress)
-    // } else {
-    //     alert('No shipping address!')
-    // }
-    fastlanePaymentComponent = await fastlane.FastlanePaymentComponent({
-      // styles,
-    });
-  } else {
-    console.log("Authentication unsuccessful... falling back to guest experience.");
-    console.groupEnd();
-    return await initializeGuestBuyerCheckout();
-  }
-  console.groupEnd();
-}
-
-async function initializeGuestBuyerCheckout() {
-  console.group("Setting up guest buyer (Gary) flow...");
-
-  const fields = {
-    phoneNumber: { prefill: "8882211161" },
-    cardholderName: { prefill: "Noauthgary Cardholder" },
-  };
-
-  const shippingAddress = {
-    name: {
-      fullName: "Kat Z.",
-    },
-    address: {
-      addressLine1: "205 E Houston St",
-      adminArea2: "New York",
-      adminArea1: "NY",
-      postalCode: "10002",
-      countryCode: "US",
-    },
-  };
-
-  const options = {
-    styles,
-    fields,
-    shippingAddress,
-  };
-  console.log("Initializing guest buyer checkout with options:", options);
-
-  fastlanePaymentComponent = await fastlane.FastlanePaymentComponent(options);
-  console.groupEnd();
-}
-
-async function initializeShippingAddress() {
-  async function changeShippingAddress() {
-    console.group("Changing shipping address...");
-    const { selectionChanged, selectedAddress } =
-      await profile.showShippingAddressSelector();
-
-    if (selectionChanged) {
-      console.log("Selection changed!");
-      displayShippingAddress(selectedAddress);
+    if (customerContextId && (await this.authenticateFastlaneMember(customerContextId))) {
+      await this.renderAcceleratedCheckout();
     } else {
-      console.log("No new address selected.");
+      await this.renderGuestCheckout();
+    }
+
+    document.getElementById("pay-button").removeAttribute("disabled");
+  }
+
+  async authenticateFastlaneMember(customerContextId) {
+    console.group("Authenticating Fastlane member...");
+    console.log({ customerContextId });
+
+    const { authenticationState, profileData } =
+      await this.fastlane.identity.triggerAuthenticationFlow(customerContextId);
+
+    console.info("Authentication result:", { authenticationState, profileData });
+    if (profileData) {
+      this.profileData = profileData;
     }
     console.groupEnd();
+    return authenticationState === "succeeded";
   }
 
-  const button = document.getElementById("shipping-address-button");
-  button.addEventListener("click", changeShippingAddress);
+  async renderGuestCheckout() {
+    console.group("Rendering guest (Gary) Fastlane flow...");
 
-  const fastlaneWatermark = await fastlane.FastlaneWatermarkComponent({
-    includeAdditionalInfo: false,
+    const fields = {
+      phoneNumber: {
+        prefill: "8882211161",
+        enabled: false,
+      },
+      cardholderName: {
+        prefill: "Noauthgary Cardholder",
+        enabled: true,
+      },
+    };
+
+    const options = { fields };
+    console.info("Config:", options);
+
+    this.cardComponent = await this.fastlane.FastlaneCardComponent(options);
+    this.cardComponent.render("#fastlane-card-container");
+    console.groupEnd();
+  }
+
+  async renderAcceleratedCheckout() {
+    console.group("Rendering accelerated (Ryan) Fastlane flow...");
+
+    /* Render
+    - the selected card from the profile object
+    - the Fastlane watermark
+    - the change card button that invokes showCardSelector()
+    */
+    console.groupEnd();
+  }
+
+  async attemptCheckout() {
+    const { id: paymentTokenId } = await this.cardComponent.getPaymentToken({
+      billingAddress: {
+        addressLine1: "2211 North 1st St",
+        adminArea1: "CA",
+        adminArea2: "San Jose",
+        postalCode: "95131",
+        countryCode: "US",
+      },
+    });
+    const { orderId, authId, captureId } = await createOrder(paymentTokenId);
+    console.log(await orderId);
+
+    if (captureId) {
+      console.log(`Received capture back from 'create order': ${captureId}`);
+    } else {
+      console.log("Attempting capture...");
+      await captureOrder({ orderId, authId });
+    }
+  }
+}
+
+window.addEventListener("load", async function () {
+  const options = loadOptions();
+  window.sessionStorage.clear();
+  setOptions(options);
+
+  const fastlane = new FastlaneFlexibleCheckout();
+  const loadFastlane = async () => {
+    await fastlane.initializeFastlane();
+
+    const emailLookupButton = document.getElementById("fastlane-email-button");
+    emailLookupButton.addEventListener("click", () => {
+      fastlane.attemptEmailLookup();
+    });
+
+    const payButton = document.getElementById("pay-button");
+    payButton.addEventListener("click", async () => {
+      await fastlane.attemptCheckout();
+    });
+  };
+
+  buildScriptElement(loadFastlane, "fastlane");
+  setupEventListeners(() => {
+    buildScriptElement(loadFastlane, "fastlane");
   });
-  fastlaneWatermark.render("#shipping-watermark-container");
-}
 
-async function displayShippingAddress(shippingAddress) {
-  const { name, address, phoneNumber } = shippingAddress;
-
-  const shippingAddressFooter = document.getElementById("shipping-watermark-container");
-  const div = document.createElement("div");
-
-  if (name) div.append(`\n${name.fullName}`);
-
-  if (address) {
-    const {
-      addressLine1 = "addressLine1",
-      addressLine2,
-      adminArea1 = "adminArea1",
-      adminArea2 = "adminArea2",
-      postalCode = "postalCode",
-    } = address;
-
-    div.append(`\n${addressLine1}`);
-
-    if (addressLine2) div.append(`\n${addressLine2}\n`);
-
-    div.append(`\n${adminArea2}, ${adminArea1} ${postalCode}\n`);
-  }
-
-  if (phoneNumber) {
-    const { countryCode = "countryCode", nationalNumber = "nationalNumber" } =
-      phoneNumber;
-    div.append(`\n+${countryCode} ${nationalNumber}\n`);
-  }
-
-  shippingAddressFooter.insertAdjacentElement("beforebegin", div);
-}
-
-async function loadFastlane() {
-  const cardOptions = { allowedBrands: ["VISA"] };
-  const fastlaneConfig = { cardOptions };
-  console.log("Instantiating paypal.Fastlane with this config:", fastlaneConfig);
-  fastlane = await paypal.Fastlane({ cardOptions });
-  console.log("Fastlane:", fastlane);
-  ({ identity, profile } = fastlane);
-
-  const fastlaneWatermark = await fastlane.FastlaneWatermarkComponent({
-    includeAdditionalInfo: true,
+  document.getElementById("ppcp-type").addEventListener("change", (event) => {
+    console.log("change", event);
+    if (document.querySelector("#ppcp-type > option[value='partner']:checked")) {
+      console.log("Partner time!");
+      saveOptionsAndReloadPage();
+    } else {
+      console.log("Direct merchant time!");
+      saveOptionsAndReloadPage("direct-merchant");
+    }
   });
-  fastlaneWatermark.render("#watermark-container");
-
-  window.localStorage.setItem("fastlaneEnv", "sandbox");
-
-  const emailLookupButton = document.getElementById("fastlane-email-button");
-  emailLookupButton.addEventListener("click", initializeCheckout);
-  emailLookupButton.classList.toggle("disabled");
-
-  document.getElementById("pay-button").addEventListener("click", attemptCheckout);
-}
-
-export { loadFastlane as default };
+});
